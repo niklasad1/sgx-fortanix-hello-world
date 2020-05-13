63d1093a06b08d6d83d38545ebd851844ecf75e8
Niklas <niklasadolfsson1@gmail.com>
2020-05-13 10:35:54 +0200

    doesn't work

--- enclave/src/main.rs
+++ enclave/src/main.rs
@@ -1,126 +1,63 @@
+#![allow(unused)]
+
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
-use std::time::{SystemTime, UNIX_EPOCH};
+// use std::time::{SystemTime, UNIX_EPOCH};
 
-use chrono::prelude::*;
+// use chrono::prelude::*;
 
-use mbedtls::hash::Type::Sha256;
-use mbedtls::pk::Pk;
-use mbedtls::rng::Rdrand;
-use mbedtls::ssl::config::{Endpoint, Preset, Transport};
-use mbedtls::ssl::{Config, Context};
-use mbedtls::x509::certificate::{Builder, Certificate};
-use mbedtls::x509::Time;
-use mbedtls::Result as TlsResult;
+use aesm_client::sgx::AesmClientExt;
+use aesm_client::AesmClient;
 
-const SOCKADDR: &str = "127.0.0.1:65000";
+use serde::ser::Serialize;
 
-const RSA_KEY_SIZE: u32 = 3072;
-const RSA_KEY_EXP: u32 = 0x10001;
-const DAYS_TO_SES: u64 = 86400;
-const CERT_VAL_SECS: u64 = 365 * DAYS_TO_SES;
+// use mbedtls::hash::Type::Sha256;
+// use mbedtls::pk::Pk;
+// use mbedtls::rng::Rdrand;
+// use mbedtls::ssl::config::{Endpoint, Preset, Transport};
+// use mbedtls::ssl::{Config, Context};
+// use mbedtls::x509::certificate::{Builder, Certificate};
+// use mbedtls::x509::Time;
+// use mbedtls::Result as TlsResult;
 
-trait ToTime {
-    fn to_time(&self) -> Time;
-}
+use sgx_isa::{Report, Targetinfo};
 
-impl ToTime for chrono::DateTime<Utc> {
-    fn to_time(&self) -> Time {
-        Time::new(
-            self.year() as _,
-            self.month() as _,
-            self.day() as _,
-            self.hour() as _,
-            self.minute() as _,
-            self.second() as _,
-        )
-        .unwrap()
-    }
-}
+const SOCKADDR: &str = "127.0.0.1:63001";
+const QUOTING_SOCKADDR: &str = "127.0.0.1:63002";
 
-fn get_validity() -> (Time, Time) {
-    let start = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    let end = start + CERT_VAL_SECS;
-    let not_before = Utc.timestamp(start as _, 0);
-    let not_after = Utc.timestamp(end as _, 0);
-    (not_before.to_time(), not_after.to_time())
-}
-
-/// The below generates a key and a self signed certificate
-/// to configure the TLS context.
-/// SGX applications should not rely on untrusted sources for their key.
-/// Ideally, enclaves communicating via TLS should, ideally,
-/// also verify attestation information.
-/// along with traditional certificate verification.
-/// But this example doesn't show that.
-fn get_key_and_cert() -> (Pk, Certificate) {
-    let mut rng = Rdrand;
-    let mut key = Pk::generate_rsa(&mut rng, RSA_KEY_SIZE, RSA_KEY_EXP).unwrap();
-    let mut key_i = Pk::generate_rsa(&mut rng, RSA_KEY_SIZE, RSA_KEY_EXP).unwrap();
-    let (not_before, not_after) = get_validity();
-
-    let cert = Certificate::from_der(
-        &Builder::new()
-            .subject_key(&mut key)
-            .subject_with_nul("CN=mbedtls-server.example\0")
-            .unwrap()
-            .issuer_key(&mut key_i)
-            .issuer_with_nul("CN=mbedtls-server.example\0")
-            .unwrap()
-            .validity(not_before, not_after)
-            .unwrap()
-            .serial(&[5])
-            .unwrap()
-            .signature_hash(Sha256)
-            .write_der_vec(&mut rng)
-            .unwrap(),
-    )
-    .unwrap();
-    (key, cert)
-}
-
-/// Establish a TLS connection with a randomly generated key and
-/// a self signed certificate.
-/// After a session is established, echo the incoming stream to the client.
-/// till EOF is detected.
-fn serve(mut conn: TcpStream, key: &mut Pk, cert: &mut Certificate) -> TlsResult<()> {
-    println!("Initiate establishing TLS session");
-
-    let mut rng = Rdrand;
-
-    let mut config = Config::new(Endpoint::Server, Transport::Stream, Preset::Default);
-    config.set_rng(Some(&mut rng));
-    config.push_cert(&mut **cert, key)?;
-    let mut ctx = Context::new(&config)?;
-
-    let mut buf = String::new();
-
-    let session = ctx.establish(&mut conn, None)?;
-    println!("Connection established!");
-    let mut reader = BufReader::new(session);
-    while let Ok(1..=std::usize::MAX) = reader.read_line(&mut buf) {
-        let session = reader.get_mut();
-        session.write_all(&buf.as_bytes()).unwrap();
-        buf.clear();
-    }
-    Ok(())
-}
+// Intel IAS keys
+const SPID: &str = "5ADBE60B563D4BC970ED2EAC0916FD72";
+const PRIMARY_KEY: &str = "e9589de0dfe5482588600a73d08b70f6";
 
 fn main() -> std::io::Result<()> {
-    let (mut key, mut cert) = get_key_and_cert();
-    let listener = TcpListener::bind(SOCKADDR).unwrap();
+    // 1. generate report, ignore manifest data for now
+    let report = Report::for_self();
+    let targetinfo = Targetinfo::from(report.clone());
+    // println!("targetinfo = {:?}", targetinfo);
+    let mut user_data = [0_u8; 64];
+    let report = Report::for_target(&targetinfo, &user_data);
 
-    println!("Enclave starting TLS Server: {}", SOCKADDR);
+    // println!("report MAC: {:?}", report.mac);
+    // println!("report KEYID: {:?}", report.keyid);
+    // println!("report CPUSVN: {:?}", report.cpusvn);
+    // println!("report MRSIGNER: {:?}", report.mrsigner);
+    // println!("report MRENCLAVE: {:?}", report.mrenclave);
 
-    for stream in listener.incoming() {
-        println!("Tcp connection received");
-        let stream = stream?;
-        let _ = serve(stream, &mut key, &mut cert).unwrap();
-        println!("Connection closed!");
-    }
+    // 2. send for quoting
+    let mut stream = TcpStream::connect(QUOTING_SOCKADDR)?;
+    stream.write_all(report.as_ref())?;
+
+    // let (mut key, mut cert) = get_key_and_cert();
+    // let listener = TcpListener::bind(SOCKADDR).unwrap();
+    //
+    // println!("Enclave starting TLS Server: {}", SOCKADDR);
+    //
+    // for stream in listener.incoming() {
+    //     println!("Tcp connection received");
+    //     let stream = stream?;
+    //     let _ = serve(stream, &mut key, &mut cert).unwrap();
+    //     println!("Connection closed!");
+    // }
 
     Ok(())
 }
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,16 +1,23 @@
-use std::io::{BufRead, BufReader, Write};
-use std::net::TcpStream;
+#![allow(unused)]
 
-use aesm_client::AesmClient;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::{thread, time};
+
+use aesm_client::{AesmClient, QuoteType};
 use enclave_runner::EnclaveBuilder;
 use sgxs_loaders::isgx::Device as IsgxDevice;
+use sgx_isa::Report;
 
-use futures::future;
-use hyper::rt::Future;
-use hyper::service::service_fn;
-use hyper::{Body, Request, Response, Server};
+// Intel IAS keys
+const SPID: &str = "5ADBE60B563D4BC970ED2EAC0916FD72";
+const PRIMARY_KEY: &str = "e9589de0dfe5482588600a73d08b70f6";
 
-type BoxFut = Box<dyn Future<Item = Response<Body>, Error = hyper::Error> + Send>;
+const ENCLAVE_SOCKADDR: &str = "127.0.0.1:63001";
+const QUOTING_SOCKADDR: &str = "127.0.0.1:63002";
+
+const SPID_SIZE: usize = 16;
+const NONCE_SIZE: usize = 16;
 
 fn usage(name: &String) {
     println!("Usage:\n{} <path_to_sgxs_file>", name);
@@ -26,16 +33,6 @@ fn parse_args() -> Result<String, ()> {
     }
 }
 
-fn fetch_enclave_randomness(_req: Request<Body>) -> BoxFut {
-    let mut stream = TcpStream::connect("127.0.0.1:65000").unwrap();
-    let _len = stream.write(b"GET SGX\r\n").unwrap();
-    let mut stream = BufReader::new(stream);
-    let mut sgx_reply = String::new();
-    stream.read_line(&mut sgx_reply).unwrap();
-
-    Box::new(future::ok(Response::new(Body::from(sgx_reply))))
-}
-
 fn main() {
     let file = parse_args().unwrap();
 
@@ -47,16 +44,42 @@ fn main() {
     let mut enclave_builder = EnclaveBuilder::new(file.as_ref());
     enclave_builder.dummy_signature();
     let enclave = enclave_builder.build(&mut device).unwrap();
+    println!("Starting Quoting TCP Server: {}", QUOTING_SOCKADDR);
 
-    // let _handle = std::thread::spawn(move || {
-    //     let addr = ([127, 0, 0, 1], 62000).into();
-    //     let server = Server::bind(&addr)
-    //         .serve(|| service_fn(fetch_enclave_randomness))
-    //         .map_err(|e| eprintln!("server error: {}", e));
-    //
-    //     println!("HTTP Server listening on http://{}", addr);
-    //     hyper::rt::run(server);
-    // });
+    let quote = thread::spawn(move || {
+        let listener = TcpListener::bind(QUOTING_SOCKADDR).expect("Tcp bind failed");
+        for stream in listener.incoming() {
+            let mut stream = stream.expect("faulty stream received");
+            let mut report = Vec::new();
+            stream.read_to_end(&mut report).unwrap();
+
+            let client = AesmClient::new();
+            let quote_info = client.init_quote().expect("init quote failed");
+            let revocation_list = Vec::new();
+            let nonce = vec![0_u8; NONCE_SIZE];
+            let spid: Vec<u8> = hex::decode(SPID).unwrap();
+
+            println!("received EREPORT: {:?}", report);
+            // println!("target_info: {:?}", quote_info);
+            // println!("SPID: {:?}", spid);
+
+            assert_eq!(spid.len(), SPID_SIZE);
+
+            let quote = client.get_quote(
+                &quote_info,
+                report,
+                spid,
+                revocation_list,
+                QuoteType::Unlinkable,
+                nonce,
+            ).expect("quoting failed");
+
+            println!("quote epid: {:?}", quote.quote());
+            println!("quote report: {:?}", quote.qe_report());
+        }
+    });
+
+    thread::sleep(time::Duration::from_millis(20));
 
     enclave
         .run()
