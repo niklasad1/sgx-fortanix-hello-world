03b6270a0dc4778134dd4275c0ce9aaff1e19880
Niklas <niklasadolfsson1@gmail.com>
2020-05-21 19:07:34 +0200

    incomplete IAS request

--- runner/src/attestation.rs
+++ runner/src/attestation.rs
@@ -1,7 +1,11 @@
 use std::net::TcpStream;
 
-use attestation_types::{EnclaveHello, ClientHello, QuoteReport};
+use attestation_types::{ClientHello, EnclaveHello, QuoteReport};
 use crypto::ephemeral_diffie_hellman::Keypair as EphemeralKeypair;
+use hyper::{body::to_bytes, client, Body, Request};
+use tokio::runtime::Runtime;
+
+const IAS_REPORT_URL: &str = "https://api.trustedservices.intel.com/sgx/dev/attestation/v4/report";
 
 pub fn attest(ephemeral_key: EphemeralKeypair, mut client_stream: TcpStream) {
     // 1. Receive `EnclaveHello`
@@ -18,11 +22,34 @@ pub fn attest(ephemeral_key: EphemeralKeypair, mut client_stream: TcpStream) {
 
     // 3. Receive `QuoteReport`
     let quote: QuoteReport = bincode::deserialize_from(&mut client_stream).unwrap();
-    println!("[CLIENT]: quote {:?}", quote);
+
+    // Send Quote report to Intel Attestation Service (IAS)
+    ias_request(quote);
 }
 
+fn ias_request(quote: QuoteReport) {
+    let client: client::Client<_, hyper::Body> =
+        client::Client::builder().build(hyper_rustls::HttpsConnector::new());
 
+    let encoded_quote = base64::encode(&quote.q);
+    let body = format!("{{\"isvEnclaveQuote\":\"{}\"}}", encoded_quote);
+    let req = Request::post(IAS_REPORT_URL)
+        .header("Content-Type", "application/json")
+        .header("Ocp-Apim-Subscription-Key","e9589de0dfe5482588600a73d08b70f6")
+        .body(Body::from(body))
+        .unwrap();
 
-fn https_client() {
+    let fut = async move {
+        let res = client.request(req).await.unwrap();
+        println!("Status:\n{}", res.status());
+        println!("Headers:\n{:#?}", res.headers());
 
+        let body: Body = res.into_body();
+        let body = to_bytes(body).await.unwrap();
+        println!("Body:\n{}", String::from_utf8_lossy(&body));
+        ()
+    };
+
+    let mut rt = Runtime::new().unwrap();
+    rt.block_on(fut);
 }
