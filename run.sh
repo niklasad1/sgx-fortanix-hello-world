956b395f7181b028596ae2357551d016e1a5962a
Niklas <niklasadolfsson1@gmail.com>
2020-05-24 23:29:28 +0200

    fix bash: kill background process on `CTRL-C`

--- run.sh
+++ run.sh
@@ -16,6 +16,7 @@ if [ ! -f $PRIVATE_KEY ]; then
     openssl rsa -in $PRIVATE_KEY -pubout > $PUBLIC_KEY
 fi
 
+trap 'kill $BGPID; exit' INT
 cargo build -p service-provider --release
 cargo build -p client --release
 cargo build -p enclave --target=x86_64-fortanix-unknown-sgx --release
@@ -23,5 +24,5 @@ ftxsgx-elf2sgxs $TARGET --heap-size 0x20000 --stack-size 0x20000 --threads 6 --d
 # sign the enclave
 sgxs-sign --key $PRIVATE_KEY $TARGET_SGXS $TARGET_SIGN -d --xfrm 7/0 --isvprodid 0 --isvsvn 0
 $CLIENT $TARGET_SGXS $TARGET_SIGN &
+BGPID=$!
 $SERVICE_PROVIDER
-trap "killall background" EXIT
